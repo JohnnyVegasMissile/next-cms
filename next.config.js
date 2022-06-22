1a29d9bcef080b5317ecbc7197d600df9319a83c
JohnnyVegasMissile <a.tanyeres@gmail.com>
2022-06-22 20:06:49 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+      REVALIDATE_TOKEN: 'U2Lzbw3exgsStKgRKzt6pq1JdPVNeLQAVHqlvdv9kL4iTzMbu38FvC9aNXBUQfkO',
+  },
 }
 
 module.exports = nextConfig

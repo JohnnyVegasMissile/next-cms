d064fb8a82f7b4fb5e8a0664aa7039ae68866d08
Alexandre Tanyeres <alexandre@mediasia-interactive.com>
2022-06-28 18:04:35 +0800

    styles to block

--- Blocks/Example/edit.tsx
+++ Blocks/Example/edit.tsx
@@ -1,5 +1,14 @@
-const Edit = () => {
-    return <section>Edit</section>
+import { useState } from 'react'
+import styles from './example.module.scss'
+interface Props {
+    defaultValues: string
+    onChange: (values: string) => void
+}
+
+const Edit = ({ defaultValues, onChange }: Props) => {
+    const [values, setValues] = useState(JSON.parse(defaultValues))
+
+    return <section className={styles.section}>Edit</section>
 }
 
 export default Edit
--- Blocks/Example/view.tsx
+++ Blocks/Example/view.tsx
@@ -1,5 +1,7 @@
+import styles from './example.module.scss'
+
 const View = () => {
-    return <section>View</section>
+    return <section className={styles.section}>View</section>
 }
 
 export default View

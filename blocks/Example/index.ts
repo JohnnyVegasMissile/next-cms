57ff6dfbac78f649e5e160058836de3c4a08c394
Alexandre Tanyeres <alexandre@mediasia-interactive.com>
2022-07-01 10:28:58 +0800

    blocks back

---
+++ blocks/Example/index.ts
@@ -0,0 +1,12 @@
+import View from './View'
+import Edit from './Edit'
+import type { Block } from '../types'
+
+const Example: Block = {
+    name: 'Example',
+    preview: '',
+    View,
+    Edit,
+}
+
+export default Example

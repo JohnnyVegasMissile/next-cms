d84307a921143af4d329be3d571380557f309f75
Alexandre Tanyeres <alexandre@mediasia-interactive.com>
2023-03-22 20:24:56 +0800

    starting the new field on container

--- components/ContainerFieldsManager/index.tsx
+++ components/ContainerFieldsManager/index.tsx
@@ -27,7 +27,7 @@ import {
     InfoCircleOutlined,
 } from '@ant-design/icons'
 import MetadatasList from '~/components/MetadatasList'
-import { ContainerFieldCreation } from '~/types/containerCreation'
+import ContainerCreation, { ContainerFieldCreation } from '~/types/containerCreation'
 import SlugEdit from '~/components/SlugEdit'
 import { ContainerFieldType } from '@prisma/client'
 import { Options } from '~/types'
@@ -38,89 +38,172 @@ import WithLabel from '~/components/WithLabel'
 const { Panel } = Collapse
 const { Text } = Typography
 
+const fieldOptions = [
+    { label: 'Text', value: ContainerFieldType.STRING },
+    { label: 'Date', value: ContainerFieldType.DATE },
+    { label: 'Boolean', value: ContainerFieldType.BOOLEAN },
+    { label: 'Number', value: ContainerFieldType.NUMBER },
+    { label: 'Link', value: ContainerFieldType.LINK },
+    { label: 'Paragraph', value: ContainerFieldType.PARAGRAPH },
+    { label: 'Image', value: ContainerFieldType.IMAGE },
+    { label: 'File', value: ContainerFieldType.FILE },
+    { label: 'Video', value: ContainerFieldType.VIDEO },
+    { label: 'Content', value: ContainerFieldType.CONTENT },
+    { label: 'Option', value: ContainerFieldType.OPTION },
+    { label: 'Rich text', value: ContainerFieldType.RICHTEXT },
+    { label: 'Color', value: ContainerFieldType.COLOR },
+    { label: 'Location', value: ContainerFieldType.LOCATION },
+]
+
+interface ContainerFieldsProps {
+    value: ContainerCreation['fields']
+    onChange(name: string, value: any): void
+    errors: any[]
+}
+
+const ContainerFieldsManager = ({ value, onChange, errors }: ContainerFieldsProps) => {
+    const addField = (type: ContainerFieldType) =>
+        onChange('', [
+            ...value,
+            {
+                tempId: tempId(),
+                name: '',
+                required: false,
+                type,
+                multiple: false,
+                options: [],
+                position: value.length,
+            },
+        ])
+
+    const deleteField = (idx: number) => {
+        const fieldsCopy = [...value]
+        fieldsCopy.splice(idx, 1)
+        onChange('', fieldsCopy)
+    }
+
+    const moveField = (idx: number, direction: 'up' | 'down') => {
+        let fieldsCopy = [...value]
+        const temp = fieldsCopy[idx]!
+        if (direction === 'up') {
+            fieldsCopy[idx] = fieldsCopy[idx + 1]!
+            fieldsCopy[idx + 1] = temp
+
+            fieldsCopy[idx]!.position = fieldsCopy[idx]!.position - 1
+            fieldsCopy[idx + 1]!.position = fieldsCopy[idx + 1]!.position + 1
+        } else {
+            fieldsCopy[idx] = fieldsCopy[idx - 1]!
+            fieldsCopy[idx - 1] = temp
+
+            fieldsCopy[idx]!.position = fieldsCopy[idx]!.position + 1
+            fieldsCopy[idx - 1]!.position = fieldsCopy[idx - 1]!.position - 1
+        }
+
+        onChange('', fieldsCopy)
+    }
+
+    const items = fieldOptions.map((e) => ({
+        label: e.label,
+        key: e.value,
+        onClick: () => addField(e.value),
+    }))
+
+    return (
+        <Space direction="vertical" style={{ width: '100%' }}>
+            {value.map((field, idx) => (
+                <Collapse
+                    size="small"
+                    key={field.id || field.tempId || `field-${idx}`}
+                    defaultActiveKey={field.tempId}
+                >
+                    <Panel
+                        key={field.id || field.tempId || `field-${idx}`}
+                        header={
+                            <Space>
+                                <div onClick={(e) => e.stopPropagation()}>
+                                    <Switch
+                                        size="small"
+                                        disabled={!!field.id}
+                                        checked={field.multiple}
+                                        onChange={(e) => onChange(`${idx}.multiple`, e)}
+                                        checkedChildren="Multiple"
+                                        unCheckedChildren="Single"
+                                    />
+                                </div>
+                                <Text strong>
+                                    {fieldOptions.find((e) => e.value === field.type)?.label} field:{' '}
+                                    {field.position}
+                                </Text>
+                                <Text>{field.name}</Text>
+                            </Space>
+                        }
+                        extra={
+                            <PopOptions
+                                onUp={() => moveField(idx, 'up')}
+                                disableUp={idx === 0}
+                                onDown={() => moveField(idx, 'down')}
+                                disableDown={idx === value.length - 1}
+                                onDelete={() => deleteField(idx)}
+                                alert={!!field.id}
+                            >
+                                <Button type="ghost" size="small" icon={<EllipsisOutlined />} />
+                            </PopOptions>
+                        }
+                    >
+                        <ContainerFields
+                            field={field}
+                            onChange={(name, value) => onChange(`${idx}.${name}`, value)}
+                        />
+                    </Panel>
+                </Collapse>
+            ))}
+
+            <Dropdown menu={{ items }}>
+                <Button size="small" type="primary" icon={<PlusOutlined />}>
+                    Add field
+                </Button>
+            </Dropdown>
+        </Space>
+    )
+}
+
 interface ContainerFieldProps {
     field: ContainerFieldCreation
     onChange(name: string, value: any): void
-    onMoveUp(): void
-    onMoveDown(): void
-    onDelete(): void
-    isFirst?: boolean
-    isLast?: boolean
 }
 
-const ContainerFields = ({
-    field,
-    onChange,
-    onMoveUp,
-    onMoveDown,
-    onDelete,
-    isFirst,
-    isLast,
-}: ContainerFieldProps) => {
+const ContainerFields = ({ field, onChange }: ContainerFieldProps) => {
     return (
-        <Collapse size="small" defaultActiveKey={field.tempId}>
-            <Panel
-                key={(field.id || field.tempId)!}
-                header={
-                    <Space>
-                        <div onClick={(e) => e.stopPropagation()}>
-                            <Switch
-                                size="small"
-                                disabled={!!field.id}
-                                checked={field.multiple}
-                                onChange={(e) => onChange(`${idx}.multiple`, e)}
-                                checkedChildren="Multiple"
-                                unCheckedChildren="Single"
-                            />
-                        </div>
-                        <Text strong>
-                            {fieldOptions.find((e) => e.value === field.type)?.label} field: {field.position}
-                        </Text>
-                        <Text>{field.name}</Text>
-                    </Space>
-                }
-                extra={
-                    <PopOptions
-                        onUp={onMoveUp}
-                        disableUp={isFirst}
-                        onDown={onMoveDown}
-                        disableDown={isLast}
-                        onDelete={onDelete}
-                        alert={!!field.id}
-                    >
-                        <Button type="ghost" size="small" icon={<EllipsisOutlined />} />
-                    </PopOptions>
-                }
-            >
-                <Row gutter={[16, 16]}>
-                    <Col span={12}>
-                        <WithLabel
-                            label={
-                                <Space>
-                                    <Text type="secondary">Name :</Text>
+        <>
+            <Row gutter={[16, 16]}>
+                <Col span={12}>
+                    <WithLabel
+                        label={
+                            <Space>
+                                <Text type="secondary">Name :</Text>
 
-                                    <Switch
-                                        size="small"
-                                        checked={field.required}
-                                        onChange={(e) => onChange(`${idx}.required`, e)}
-                                        checkedChildren="Required"
-                                        unCheckedChildren="Optional"
-                                    />
-                                </Space>
-                            }
-                            error="Name is required"
-                        >
-                            <Input
-                                size="small"
-                                status="error"
-                                style={{ width: '100%' }}
-                                value={field.name}
-                                onChange={(e) => onChange(`${idx}.name`, e.target.value)}
-                            />
-                        </WithLabel>
-                    </Col>
-                </Row>
-                {/* <Row gutter={[16, 16]}>
+                                <Switch
+                                    size="small"
+                                    checked={field.required}
+                                    onChange={(e) => onChange('required', e)}
+                                    checkedChildren="Required"
+                                    unCheckedChildren="Optional"
+                                />
+                            </Space>
+                        }
+                        error="Name is required"
+                    >
+                        <Input
+                            size="small"
+                            status="error"
+                            style={{ width: '100%' }}
+                            value={field.name}
+                            onChange={(e) => onChange('name', e.target.value)}
+                        />
+                    </WithLabel>
+                </Col>
+            </Row>
+            {/* <Row gutter={[16, 16]}>
                             <Col span={16}>
                                 <Row gutter={[16, 16]}>
                                     <Col span={12}>
@@ -294,7 +377,7 @@ const ContainerFields = ({
                             )}
                         </Row> */}
 
-                {/* <Space direction="vertical">
+            {/* <Space direction="vertical">
                             <Space></Space>
 
                             <Space>
@@ -305,9 +388,8 @@ const ContainerFields = ({
                 </Space>
                             </Space>
                         </Space> */}
-            </Panel>
-        </Collapse>
+        </>
     )
 }
 
-export default ContainerFields
+export default ContainerFieldsManager
